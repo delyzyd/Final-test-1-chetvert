33743efb67fbd5e724005cbe0ebec89f53933c98
Anton Mukhortyihk <mtk12ab@gmail.com>
2023-03-28 16:08:09 +0300

    Написал програмку

--- blockcxema/work programm/Program.cs
+++ blockcxema/work programm/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array) // Создание массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i] + " ");
+    }
+    System.Console.WriteLine(); //Пустая строчка
+}
+
+System.Console.WriteLine("Количество элементов в массиве ");
+int size = Convert.ToInt32(Console.ReadLine());// Заполняем массив
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    System.Console.WriteLine($"Введите {i + 1} элемент: ");
+    string symbol = Console.ReadLine()!;
+    arrayStrings[i] = symbol;
+}
+
+string[] arrayFinal = new string[size];
+int length = 3;
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (arrayStrings[i].Length <= length)
+    {
+        arrayFinal[count] = arrayStrings[i];
+        count++;
+    }
+}
+
+System.Console.WriteLine();
+PrintArray(arrayFinal);
